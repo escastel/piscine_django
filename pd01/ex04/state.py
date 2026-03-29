607be9abb7e5bb223a75d155d8471bfd93ec76aa
escastel <escastel@student.42malaga.com>
2026-03-29 19:27:41 +0200

    [ex04]: diccionario de capitales y estados al reves completado

---
+++ pd01/ex04/state.py
@@ -0,0 +1,37 @@
+import sys
+
+def rev_capital_city(capital):
+    states = {
+        "Oregon" : "OR",
+        "Alabama" : "AL",
+        "New Jersey": "NJ",
+        "Colorado" : "CO"
+    }
+
+    capital_cities = {
+        "OR": "Salem",
+        "AL": "Montgomery",
+        "NJ": "Trenton",
+        "CO": "Denver"
+    }
+    
+    value_state = ""
+    for key, value in capital_cities.items():
+        if value == capital:
+            value_state = key
+
+    if not value_state:
+       print("Unknown capital city")
+       sys.exit(1)
+
+    for key, value in states.items():
+        if value == value_state:
+            print(key)
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit(1)
+    rev_capital_city(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
