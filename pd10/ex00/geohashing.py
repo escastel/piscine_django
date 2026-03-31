a4394d3e1a8951160a602bc23e4b55a471edd72b
escastel <escastel@student.42malaga.com>
2026-03-31 13:58:09 +0200

    [ex00]: completado el parseo y programa

--- pd10/ex00/geohashing.py
+++ pd10/ex00/geohashing.py
@@ -1,3 +1,4 @@
+import antigravity as a
 import sys
 
 def parse_date(year, month, day):
@@ -21,11 +22,11 @@ def parse_float():
 
     return latitude, longitude
 
-def parse_int(datedow):
+def parse_int(dtdw):
     try:
-        year = int(datedow[0])
-        month = int(datedow[1])
-        day = int(datedow[2])
+        year = int(dtdw[0])
+        month = int(dtdw[1])
+        day = int(dtdw[2])
     except Exception:
         print(f"\033[31mError: the third argument has no format YYYY-MM-DD-DOW.\033[0m")
         sys.exit(1)
@@ -35,22 +36,34 @@ def parse_int(datedow):
 def parse():
     latitude, longitude = parse_float()
     
-    datedow = sys.argv[3].split("-")
-    if len(datedow) != 4 or len(datedow[0]) != 4 or len(datedow[1]) != 2 or len(datedow[2]) != 2:
+    dtdw = sys.argv[3].split("-")
+    if len(dtdw) != 4 or len(dtdw[0]) != 4 or len(dtdw[1]) != 2 or len(dtdw[2]) != 2:
         print(f"\033[31mError: the third argument has no format YYYY-MM-DD-DOW.\033[0m")
         sys.exit(1)
     
-    year, month, day = parse_int(datedow)
-
+    year, month, day = parse_int(dtdw)
     if not parse_date(year, month, day):
         print(f"\033[31mError: the third argument has no format YYYY-MM-DD-DOW.\033[0m")
         sys.exit(1)
+
+    try:
+        float(dtdw[3])
+    except ValueError:
+       print(f"\033[31mError: the third argument has no format YYYY-MM-DD-DOW.\033[0m")
+       sys.exit(1)
+    
+    datedow = "-".join(dtdw).encode()
+    return latitude, longitude, datedow
+
 
 
 def main():
     if len(sys.argv) != 4:
         print("\033[31mError: invalid number of arguments.\033[0m")
         sys.exit(1)
-    parse()
+
+    latitude, longitude, datedow = parse()
+    a.geohash(latitude, longitude, datedow)
+
 if __name__ == '__main__':
     main()
